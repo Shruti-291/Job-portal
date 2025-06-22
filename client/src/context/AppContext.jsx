59c1b7b4ceb83fbc3c44a1c479aa80b2d9d25db4
Shruti Kumari <shrutiblkumari@gmail.com>
2025-06-22 19:24:48 +0530

    implemented remaining functionalities

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,17 @@
 import { createContext, useEffect } from "react";
 import { useState } from "react";
 export const AppContext = createContext();
-import { jobsData } from "../assets/assets";
+import { useAuth } from "@clerk/clerk-react";
+import {toast} from 'react-toastify'
+import axios from 'axios'
+import { useUser } from "@clerk/clerk-react";
+
 export const AppContextProvider = (props)=>{
 
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const {user}=useUser()
+    const {getToken} = useAuth()
     const [searchFilter,setSearchFilter] = useState({
         title:"",
         location:""
@@ -15,14 +23,103 @@ export const AppContextProvider = (props)=>{
 
     const [showRecruiterLogin,setShowRecruiterLogin] = useState(false)
 
+    const [companyToken,setCompanyToken] = useState(null)
+    const [companyData,setCompanyData]=useState(null)
+
+    const [userData,setUserData] = useState(null)
+    const [userApplication,setUserApplication] = useState([])
+
     //Function to fetch jobs data
     const fetchJobs =  async()=>{
-        setJobs(jobsData)
+        try{
+            const {data} = await axios.get(backendUrl+'/api/jobs')
+            if(data.success){
+                setJobs(data.jobs)
+                console.log(data.jobs)
+            }
+            else{
+                toast.error(data.message)
+            }
+        }catch(error){
+            toast.error(error.message)
+        }
+       
+    }
+
+    //Function to fetch company data
+    const fetchCompanyData = async()=>{
+        try{
+
+            const {data} = await axios.get(backendUrl+'/api/company/company',{headers:{token:companyToken}})
+            if(data.success){
+                setCompanyData(data.company)
+                console.log(data);
+            }
+            else{
+                toast.error(data.message)
+            }
+        }catch(error){
+            toast.error(error.message)
+        }
+    }
+
+    //Function to fetch user data
+    const fetchUserData = async()=>{
+        try {
+            const token = await getToken();
+            const {data} = await axios.get(backendUrl+'/api/users/user',{headers:{Authorization:`Bearer ${token}`}})
+
+            if(data.success){
+                setUserData(data.user)
+            }
+            else{
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    //Function to fetch user's applied applications data
+    const fetchUserApplications = async()=>{
+        try {
+            const token = await getToken()
+            const {data} = await axios.get(backendUrl+'/api/users/applications',{headers:{Authorization:`Bearer ${token}`}})
+
+            if(data.success){
+                setUserApplication(data.applications)
+            }
+            else{
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
     }
     useEffect(()=>{
         fetchJobs()
+        const storedCompanyToken = localStorage.getItem('companyToken')
+        if(storedCompanyToken){
+            setCompanyToken(storedCompanyToken)
+        }
     },[])
     
+    useEffect(()=>{
+
+        if(companyToken){
+            fetchCompanyData()
+        }
+    },[companyToken])
+
+
+    useEffect(()=>{
+        if(user){
+            fetchUserData()
+            fetchUserApplications()
+        }
+    },[user])
     const val = {
         searchFilter,
         setSearchFilter,
@@ -30,6 +127,11 @@ export const AppContextProvider = (props)=>{
         setIsSearched,
         jobs,setJobs,
         showRecruiterLogin,setShowRecruiterLogin,
+        companyToken,setCompanyToken,
+        companyData,setCompanyData,
+        backendUrl,userData,setUserData,
+        userApplication,setUserApplication,
+        fetchUserData,fetchUserApplications,
         
     }
     return (<AppContext.Provider value={val}>
